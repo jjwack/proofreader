f86b06c81d7d3000b663ab4bb7b7596245131df0
Ethan Soergel <esoergel@gmail.com>
2013-06-20 13:51:12 -0400

    changed project structure

--- proofread/settings.py
+++ proofread/settings.py
@@ -89,10 +89,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'proofread.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'proofread.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
