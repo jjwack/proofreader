ca8e69ad82bc8014c6d7920992c35a8fae861ca8
Ethan Soergel <esoergel@gmail.com>
2013-06-20 16:55:41 -0400

    updated views some

--- proofread/api/views.py
+++ proofread/api/views.py
@@ -1,10 +1,27 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 
 from braces.views import JSONResponseMixin, AjaxResponseMixin
 
 class MainPage(TemplateView):
     template_name = "marketing/home.html"
 
-class ESLReceipt(JSONResponseMixin, AjaxResponseMixin, TemplateView):
+class ESLReceipt(TemplateView):
     template_name = "esl_receipt/response.html"
-    content_type = "text/html" # this needs to be conditional on the request method
+
+    def get(self, request, *args, **kwargs):
+        context = {'response': 'You must POST a message'}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        context = {'response': 'Success!'}
+        return self.render_to_response(context)
+
+
+class ESLReceiptAPI(JSONResponseMixin, AjaxResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        context = {'success': 0, 'message': 'You must POST a message'}
+        return self.render_json_response(context)
+
+    def post(self, request, *args, **kwargs):
+        context = {'success': 1, 'message': "We got it!  You'll hear back soon"}
+        return self.render_json_response(context)
--- proofread/urls.py
+++ proofread/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from api.views import MainPage, ESLReceipt
+from api.views import MainPage, ESLReceipt, ESLReceiptAPI
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', MainPage.as_view(), name="main"),
-    url(r'^api/', ESLReceipt.as_view(), name='api'),
+    url(r'^dashboard/$', TemplateView.as_view(template_name="dashboard.html"), name="dashboard"),
+    url(r'^submit_message/$', ESLReceipt.as_view(content_type="text/html"), name='submit_message'),
+    url(r'^api/$', ESLReceiptAPI.as_view(content_type="application/json"), name='api'),
+
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
