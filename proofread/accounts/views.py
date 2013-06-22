5bc253471b05a52fc4ddb416efb0dbbbf013f9f4
Ethan Soergel <esoergel@gmail.com>
2013-06-21 23:33:01 -0400

    added a couple things to the stripe view

--- proofread/accounts/views.py
+++ proofread/accounts/views.py
@@ -1,8 +1,14 @@
 from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+
 
 class PayStripe(TemplateView):
     template_name = "main.html"
 
-    def POST(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
+        print self.request.POST
         # add stripe stuff
-        return render_to_response({})
+        # check the terminal output; self.request.POST is a dictionary
+        # containing all the POST parameters.
+        stripe_token = self.request.POST['stripeToken']
+        return HttpResponseRedirect('/')
