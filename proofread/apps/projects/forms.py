6353d41a297249ca32d45c9660450cbe59e3484d
Ethan Soergel <esoergel@gmail.com>
2013-06-27 18:33:46 -0400

    prettied up some more stuff

--- proofread/apps/projects/forms.py
+++ proofread/apps/projects/forms.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
 from django import forms
 
 from .models import Project
 
 
 class ProjectForm(forms.ModelForm):
+    now = datetime.strftime(datetime.now(), "%B %d, %Y %I:%M %p")
+    title = forms.CharField(initial=now)
 
     class Meta:
         model = Project
